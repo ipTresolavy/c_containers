ce0cb2782ae6ead049508a3e566e6af90657c5d2
Igor Pontes Tresolavy <tresolavy@usp.br>
2024-08-23 04:00:17 -0300

    refactor: adds TODO comment

--- include/operator.h
+++ include/operator.h
@@ -1,5 +1,11 @@
 #ifndef H_OPERATOR
 #define H_OPERATOR
+// TODO: create general return types for operators and specific ones for arrays
+// check how CMake can generate source files with macros expanded (one for each .c)
+// maybe generalize the __PREFIX__ macro
+// doc: explain what the project is (a bunch of macros) and usage
+// begin with end result and explain what user has to do to use it
+// check if it would be better to destruct the array elements on the reverse order, without using deinit
 
 #define __CONCAT__(x, y)   x##y
 #define CONCAT(x, y)       __CONCAT__(x, y)
