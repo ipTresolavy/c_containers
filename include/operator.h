5412c81558c5a68635cdf722ba661b8d0fa6f4f2
Igor Pontes Tresolavy <tresolavy@usp.br>
2024-08-27 00:22:04 -0300

    wip: expands TODO comment

--- include/operator.h
+++ include/operator.h
@@ -2,9 +2,10 @@
 #define H_OPERATOR
 // TODO: create general return types for operators and specific ones for arrays
 // check how CMake can generate source files with macros expanded (one for each
-// .c) maybe generalize the CONT_PRIV_PREFIX macro doc: explain what the project
-// is (a bunch of macros) and usage. Begin with end result and explain what user
-// has to do to use it. Also say it is type-safe
+// .c) maybe generalize the CONT_PRIV_PREFIX macro si that it can be used with other containers
+// doc: explain what the project is (a bunch of macros) and usage. Begin with end result and explain what user
+// has to do to use it. Also say it is type-safe and can be used with opaque types (you need to know the definition of
+// the types to implement the containers, but not to use them)
 
 #define CONT_PRIV_CONCAT(x, y)   x##y
 #define CONT_CONCAT(x, y)        CONT_PRIV_CONCAT(x, y)
